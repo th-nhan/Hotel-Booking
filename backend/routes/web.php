0562b2359e37180022664879338b52a4a951a2f3
th-nhan <tthhaannnnhhaann@gmail.com>
2026-03-05 16:28:00 +0700

    Them luoi quet URL thuc te

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Tuyệt chiêu gom mọi đường link: Bất kể gõ link gì cũng sẽ lọt vào đây
+Route::any('{any}', function (Request $request) {
+    return response()->json([
+        'tin_nhan' => 'Đã tóm được URL thực tế!',
+        'url_laravel_dang_hieu_la' => $request->path(),
+        'phuong_thuc' => $request->method()
+    ]);
+})->where('any', '.*');
